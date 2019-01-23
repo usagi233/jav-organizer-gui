f9c14d6ff79574cb96fd008be5dd87c50ff31891
usagi233 <animeistrash233@gmail.com>
2019-01-24 01:47:51 +0900

    First iteration

---
+++ jav-organizer-gui/MainForm.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace jav_organizer_gui
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.PathTextBox = new System.Windows.Forms.TextBox();
+            this.BrowseButton = new System.Windows.Forms.Button();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.LogTextBox = new System.Windows.Forms.TextBox();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // PathTextBox
+            // 
+            this.PathTextBox.Location = new System.Drawing.Point(10, 10);
+            this.PathTextBox.Name = "PathTextBox";
+            this.PathTextBox.Size = new System.Drawing.Size(360, 20);
+            this.PathTextBox.TabIndex = 0;
+            // 
+            // BrowseButton
+            // 
+            this.BrowseButton.Location = new System.Drawing.Point(60, 36);
+            this.BrowseButton.Name = "BrowseButton";
+            this.BrowseButton.Size = new System.Drawing.Size(75, 25);
+            this.BrowseButton.TabIndex = 1;
+            this.BrowseButton.Text = "Browse";
+            this.BrowseButton.UseVisualStyleBackColor = true;
+            this.BrowseButton.Click += new System.EventHandler(this.BrowseButton_Click);
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(260, 36);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(75, 25);
+            this.StartButton.TabIndex = 3;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // LogTextBox
+            // 
+            this.LogTextBox.Location = new System.Drawing.Point(13, 65);
+            this.LogTextBox.Multiline = true;
+            this.LogTextBox.Name = "LogTextBox";
+            this.LogTextBox.ReadOnly = true;
+            this.LogTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.LogTextBox.Size = new System.Drawing.Size(358, 184);
+            this.LogTextBox.TabIndex = 4;
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Location = new System.Drawing.Point(160, 36);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(75, 25);
+            this.ClearButton.TabIndex = 2;
+            this.ClearButton.Text = "Clear";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 261);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.LogTextBox);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.BrowseButton);
+            this.Controls.Add(this.PathTextBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(400, 300);
+            this.MinimumSize = new System.Drawing.Size(400, 300);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Jav Organizer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox PathTextBox;
+        private System.Windows.Forms.Button BrowseButton;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.TextBox LogTextBox;
+        private System.Windows.Forms.Button ClearButton;
+    }
+}
+
---
+++ jav-organizer-gui/MainForm.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+
+namespace jav_organizer_gui
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void BrowseButton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                PathTextBox.Text = folderBrowserDialog.SelectedPath;
+            }
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            PathTextBox.Text = "";
+            LogTextBox.Text = "";
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            string path = PathTextBox.Text;
+            if (Directory.Exists(path))
+            {
+                ToggleComponents(false);
+                Process process = new Process();
+                process.StartInfo.FileName = Application.StartupPath + "\\jav-organizer.exe";
+                process.StartInfo.Arguments = "-d " + path;
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.CreateNoWindow = true;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.OutputDataReceived += new DataReceivedEventHandler(OutputHandler);
+                process.Exited += OnProcessExit;
+                process.EnableRaisingEvents = true;
+                try
+                {
+                    process.Start();
+                }
+                catch 
+                {
+                    MessageBox.Show("Cannot run jav-organizer executable", "Error");
+                    ToggleComponents(true);
+                    return;
+                }
+                process.BeginOutputReadLine();
+                
+            }
+            else
+            {
+                MessageBox.Show("\"" + path + "\" is not a valid directory.","Error");
+            }
+        }
+
+        delegate void BoolArgReturnVoidDelegate(bool value);
+        private void ToggleComponents(bool enabled)
+        {
+            if (PathTextBox.InvokeRequired)
+            {
+                BoolArgReturnVoidDelegate d = new BoolArgReturnVoidDelegate(ToggleComponents);
+                Invoke(d, new object[] { enabled });
+            }
+            else
+            {
+                PathTextBox.Enabled = enabled;
+                BrowseButton.Enabled = enabled;
+                ClearButton.Enabled = enabled;
+                StartButton.Enabled = enabled;
+            }
+        }
+        
+        private void OnProcessExit(object sender, EventArgs e)
+        {
+            ToggleComponents(true);
+        }
+
+        private void OutputHandler(object sender, DataReceivedEventArgs outLine)
+        {
+            if (outLine.Data != null)
+            {
+                byte[] bytes = Encoding.Default.GetBytes(outLine.Data);
+                string decoded = Encoding.UTF8.GetString(bytes);
+                LogOutput(decoded);
+            }
+        }
+
+        delegate void StringArgReturningVoidDelegate(string text);
+        private void LogOutput(string text)
+        {
+            if (LogTextBox.InvokeRequired)
+            {
+                StringArgReturningVoidDelegate d = new StringArgReturningVoidDelegate(LogOutput);
+                Invoke(d, new object[] { text });
+            }
+            else
+            {
+                LogTextBox.AppendText(text + "\r\n");
+            }
+        }
+        
+    }
+}
